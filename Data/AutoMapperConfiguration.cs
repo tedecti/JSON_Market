eb21451ece0af1f87097abde78fdb145dddc46d9
tedecti <wolftheguy08@gmail.com>
2024-06-13 22:50:46 +0500

    AutoMapper config

---
+++ Data/AutoMapperConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using JSON_Market.Models.Order;
+using JSON_Market.Models.Order.GET;
+using JSON_Market.Models.Product;
+using JSON_Market.Models.Product.GET;
+
+namespace JSON_Market.Data;
+
+public class AutoMapperConfiguration : Profile
+{
+    public AutoMapperConfiguration()
+    {
+        CreateMap<Product, GetAllProductsDto>();
+        CreateMap<Product, GetAllProductsBySellerDto>();
+        CreateMap<Order, GetAllOrdersByCustomerDto>();
+    }
+}
