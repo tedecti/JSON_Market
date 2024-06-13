061518fad0e93a88ab7de4984e958b37b023f35c
tedecti <wolftheguy08@gmail.com>
2024-06-13 17:54:43 +0500

    Initialize database context

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,27 @@
+﻿using JSON_Market.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace JSON_Market.Data;
+
+public class AppDbContext : DbContext
+{
+    private readonly IConfiguration _configuration;
+    public AppDbContext(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseNpgsql(_configuration.GetConnectionString("Npgsql"));
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Customer>()
+            .HasMany(c=>c.OrderHistory)
+            .WithOne(c=>c.Customer);
+    }
+
+    public DbSet<Customer> Customers;
+}
