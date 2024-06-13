f1418fa29bb0ab64b4dfcda7fd2a542dd6ba8b46
tedecti <wolftheguy08@gmail.com>
2024-06-13 22:50:35 +0500

    Product + Order DTOs

---
+++ Models/Order/GET/GetAllOrdersByCustomerDto.cs
@@ -0,0 +1,7 @@
+﻿namespace JSON_Market.Models.Order.GET;
+
+public class GetAllOrdersByCustomerDto
+{
+    public string? Name { get; set; }
+    public Order Order { get; set; }
+}
---
+++ Models/Product/GET/GetAllProductsDto.cs
@@ -0,0 +1,11 @@
+﻿namespace JSON_Market.Models.Product.GET;
+
+public class GetAllProductsDto
+{
+    public string? Title { get; set; }
+    public string? Description { get; set; }
+    public ushort Price { get; set; }
+    public byte Discount { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public List<string> ImageUrls { get; set; } = [];
+}
