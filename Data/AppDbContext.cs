e7633176764f1b6191020fff491ab1c6d79fc8a5
tedecti <wolftheguy08@gmail.com>
2024-06-13 18:27:21 +0500

    DbContext use extensions + DbSets (Orders, Products, Sellers)

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -6,6 +6,7 @@ namespace JSON_Market.Data;
 public class AppDbContext : DbContext
 {
     private readonly IConfiguration _configuration;
+
     public AppDbContext(IConfiguration configuration)
     {
         _configuration = configuration;
@@ -18,10 +19,19 @@ public class AppDbContext : DbContext
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        modelBuilder.Entity<Customer>()
-            .HasMany(c=>c.OrderHistory)
-            .WithOne(c=>c.Customer);
+        modelBuilder.ConfigureHasManyWithOne<Customer, Order>(c => c.OrderHistory,
+            o => o.Customer,
+            o => o.CustomerId,
+            DeleteBehavior.Cascade);
+        modelBuilder.ConfigureHasManyWithOne<Seller, Product>(s => s.CreatedProducts,
+            p => p.Seller,
+            p => p.SellerId,
+            DeleteBehavior.Cascade);
+        modelBuilder.Entity<Order>().HasMany(o => o.Products);
     }
 
     public DbSet<Customer> Customers;
+    public DbSet<Order> Orders;
+    public DbSet<Product> Products;
+    public DbSet<Seller> Sellers;
 }
