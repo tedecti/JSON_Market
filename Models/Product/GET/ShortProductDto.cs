bfb1d7040434e75d47b104ade2fe1af35e64a431
tedecti <wolftheguy08@gmail.com>
2024-06-14 14:20:49 +0500

    short dtos

---
+++ Models/Product/GET/ShortProductDto.cs
@@ -0,0 +1,11 @@
+﻿namespace JSON_Market.Models.Product.GET;
+
+public class ShortProductDto
+{
+    public Guid Id { get; set; }
+    public string Title { get; set; }
+    public string? Description { get; set; }
+
+    public decimal Price { get; set; }
+    public byte Discount { get; set; }
+}
