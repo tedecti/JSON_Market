e5e0efc8e38a48c6d9f0a1354dd54ec0aa146f8b
tedecti <wolftheguy08@gmail.com>
2024-06-14 14:23:01 +0500

    file uploadnig repository

---
+++ Controllers/FilesController.cs
@@ -0,0 +1,47 @@
+﻿using JSON_Market.Repository.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Formats.Png;
+using SixLabors.ImageSharp.Processing;
+
+namespace JSON_Market.Controllers
+{
+    [Route("api/files")]
+    [ApiController]
+    public class FilesController : ControllerBase
+    {
+        private readonly IWebHostEnvironment _environment;
+        private readonly IFileRepository _fileRepository;
+
+        public FilesController(IWebHostEnvironment environment, IFileRepository fileRepository)
+        {
+            _environment = environment;
+            _fileRepository = fileRepository;
+        }
+
+        [HttpGet("{fileName}")]
+        public async Task<IActionResult> Index(string fileName)
+        {
+            var imageStream = await _fileRepository.GetFile(fileName);
+            if (imageStream == null)
+                return NotFound();
+
+            using (var image = Image.Load(imageStream))
+            {
+                image.Mutate(x => x
+                    .Resize(new ResizeOptions
+                    {
+                        Size = new Size(375, 500),
+                        Mode = ResizeMode.Max
+                    })
+                );
+
+                var compressedImageStream = new MemoryStream();
+                image.Save(compressedImageStream, new PngEncoder());
+                compressedImageStream.Seek(0, SeekOrigin.Begin);
+
+                return File(compressedImageStream, "image/png");
+            }
+        }
+    }
+}
---
+++ Repository/FileRepository.cs
@@ -0,0 +1,81 @@
+﻿using JSON_Market.Repository.Interfaces;
+
+namespace JSON_Market.Repository;
+
+public class FileRepository : IFileRepository
+{
+    private readonly IConfiguration _configuration;
+    private readonly string _localDirectory;
+
+    public FileRepository(IConfiguration configuration)
+    {
+        _configuration = configuration;
+        _localDirectory = _configuration["LocalStorage:Path"] ?? "Images";
+        if (!Directory.Exists(_localDirectory))
+        {
+            Directory.CreateDirectory(_localDirectory);
+        }
+    }
+
+    public async Task<string> SaveFile(IFormFile file)
+    {
+        var myUuid = Guid.NewGuid();
+        var fileName = $"{myUuid}.{file.ContentType.Split("/")[1]}";
+        var filePath = Path.Combine(_localDirectory, fileName);
+
+        try
+        {
+            await using var stream = new FileStream(filePath, FileMode.Create);
+            await file.CopyToAsync(stream);
+            return fileName;
+        }
+        catch (Exception e)
+        {
+            throw new ArgumentException("Ошибка при загрузке изображения");
+        }
+    }
+
+    public async Task<Stream> GetFile(string fileName)
+    {
+        var filePath = Path.Combine(_localDirectory, fileName);
+
+        if (!File.Exists(filePath))
+            return null;
+
+        try
+        {
+            var memoryStream = new MemoryStream();
+            await using (var stream = new FileStream(filePath, FileMode.Open, FileAccess.Read))
+            {
+                await stream.CopyToAsync(memoryStream);
+            }
+
+            memoryStream.Seek(0, SeekOrigin.Begin);
+            return memoryStream;
+        }
+        catch (Exception e)
+        {
+            throw new ArgumentException("Ошибка при получении изображения");
+            return null;
+        }
+    }
+
+    public async Task<bool> DeleteFileFromStorage(string fileName)
+    {
+        var filePath = Path.Combine(_localDirectory, fileName);
+
+        if (!File.Exists(filePath))
+            return false;
+
+        try
+        {
+            File.Delete(filePath);
+            return true;
+        }
+        catch (Exception e)
+        {
+            throw new ArgumentException("Ошибка при удалении изображения");
+            return false;
+        }
+    }
+}
